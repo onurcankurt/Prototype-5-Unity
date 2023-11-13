3243e1c26fe8c68a074b40e7228540ec0de7f1bc
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-13 21:19:02 +0300

    difficulty buttons added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,13 +15,12 @@ public class GameManager : MonoBehaviour
     public bool isGameActive;
     public Button restartButton;
 
+    public GameObject titleScreen;
+
     private float spawnRate = 1.0f;
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -57,6 +56,16 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
     }
 
 
